325f6c5f0dd8867da7141a847b56a300e1a9116c
Mayank Prasoon <mayank.prasoon@outlook.com>
2020-05-31 00:17:12 +0530

    ssome change

--- ImagineAR/camera/react_frontend/src/App.js
+++ ImagineAR/camera/react_frontend/src/App.js
@@ -17,6 +17,93 @@ const videoConstraints = {
   facingMode: "user"
 };
 
+const Vis = () => {
+  const { useRef, useEffect, useState } = React
+  const mount = useRef(null)
+  const [isAnimating, setAnimating] = useState(true)
+  const controls = useRef(null)
+
+  useEffect(() => {
+    let width = mount.current.clientWidth
+    let height = mount.current.clientHeight
+    let frameId
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    const geometry = new THREE.BoxGeometry(1, 1, 0.01)
+    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff, map: loader.load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg') })
+    const cube = new THREE.Mesh(geometry, material)
+
+
+    var loader = new THREE.TextureLoader()
+
+
+    camera.position.z = 4
+    scene.add(cube)
+    renderer.setClearColor('#000000')
+    renderer.setSize(width, height)
+
+    const renderScene = () => {
+      renderer.render(scene, camera)
+    }
+
+    const handleResize = () => {
+      width = mount.current.clientWidth
+      height = mount.current.clientHeight
+      renderer.setSize(width, height)
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+      renderScene()
+    }
+
+    const animate = () => {
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+
+      renderScene()
+      frameId = window.requestAnimationFrame(animate)
+    }
+
+    const start = () => {
+      if (!frameId) {
+        frameId = requestAnimationFrame(animate)
+      }
+    }
+
+    const stop = () => {
+      cancelAnimationFrame(frameId)
+      frameId = null
+    }
+
+    mount.current.appendChild(renderer.domElement)
+    window.addEventListener('resize', handleResize)
+    start()
+
+    controls.current = { start, stop }
+
+    return () => {
+      stop()
+      window.removeEventListener('resize', handleResize)
+      mount.current.removeChild(renderer.domElement)
+
+      scene.remove(cube)
+      geometry.dispose()
+      material.dispose()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isAnimating) {
+      controls.current.start()
+    } else {
+      controls.current.stop()
+    }
+  }, [isAnimating])
+
+  return <div className="vis" ref={mount} onClick={() => setAnimating(!isAnimating)} />
+}
+
 function Threerender() {
 
   const tick = React.useRef(null)
