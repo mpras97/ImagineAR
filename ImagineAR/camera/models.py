f5fd37304068329adb9696a0ebd5fc4150758cd4
shivinquartic <shivin@quartic.ai>
2020-05-30 22:34:25 +0530

    Removed unwanted files
    Signed-off-by: shivinquartic <shivin@quartic.ai>

--- ImagineAR/camera/models.py
+++ ImagineAR/camera/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Template(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
